715ad3960f99917043c290f469b1c48e140f3ef9
Rmy5 <courteaud.remy@gmail.com>
2018-05-24 16:51:26 +0200

    test db

---
+++ src/Controller/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Entity\User;
+
+class UserController extends Controller
+{
+    /**
+     * @Route("/user", name="user")
+     */
+    public function index()
+    {
+        $uManager = $this->getDoctrine()->getManager();
+
+        $user = new User();
+        $user->setPseudo('nono');
+        $user->setAge(3);
+
+        $uManager->persist($user);
+
+        $uManager->flush();
+
+	    return new Response('Saved new user with id '.$user->getId());
+    }
+}
