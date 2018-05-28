ac826d7e7159b3d7cadd7d2dc23b869cc911ab7c
Rmy5 <courteaud.remy@gmail.com>
2018-05-28 18:31:54 +0200

    Updated User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,11 +3,14 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
+class User implements UserInterface, \Serializable
 {
     /**
      * @ORM\Id()
@@ -17,9 +20,17 @@ class User
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=20)
+     * @ORM\Column(type="string", length=20, unique=true)
+     * @Assert\NotBlank()
+     * @Assert\NotBlank()
      */
-    private $pseudo;
+    private $username;
+
+	/**
+	 * @Assert\NotBlank()
+	 * @Assert\Length(max=4096)
+	 */
+	private $plainPassword;
 
     /**
      * @ORM\Column(type="string", length=120)
@@ -27,48 +38,110 @@ class User
     private $password;
 
     /**
-     * @ORM\Column(type="string", length=120)
+     * @ORM\Column(type="string", length=120, unique=true)
+     * @Assert\NotBlank())
+     * @Assert\NotBlank()
+     * @Assert\Email()
      */
     private $email;
+
+	/**
+	 * @ORM\Column(name="is_active", type="boolean")
+	 */
+	private $isActive;
+
+	/**
+	 * @ORM\Column(type="array")
+	 */
+	private $roles;
+
+	public function __construct()
+	{
+		$this->isActive = true;
+		$this->roles = array('ROLE_USER');
+	}
 
     public function getId()
     {
         return $this->id;
     }
 
-    public function getPseudo(): ?string
+    public function getUsername()
     {
-        return $this->pseudo;
+        return $this->username;
     }
 
-    public function setPseudo(string $pseudo): self
+    public function setUsername(string $username)
     {
-        $this->pseudo = $pseudo;
+        $this->username = $username;
 
         return $this;
     }
 
-    public function getPassword(): ?string
+	public function getPlainPassword()
+	{
+		return $this->plainPassword;
+	}
+
+	public function setPlainPassword( $plainPassword )
+	{
+		$this->plainPassword = $plainPassword;
+	}
+
+    public function getPassword()
     {
         return $this->password;
     }
 
-    public function setPassword(string $password): self
+    public function setPassword(string $password)
     {
         $this->password = $password;
 
         return $this;
     }
 
-    public function getEmail(): ?string
+    public function getEmail()
     {
         return $this->email;
     }
 
-    public function setEmail(string $email): self
+    public function setEmail(string $email)
     {
         $this->email = $email;
 
         return $this;
     }
+
+	public function serialize()
+	{
+		return serialize(array(
+			$this->id,
+			$this->username
+		));
+	}
+
+	public function unserialize( $serialized )
+	{
+		list (
+			$this->id,
+			$this->username
+			) = unserialize($serialized, ['allowed_classes' => false]);
+	}
+
+	public function getRoles()
+	{
+		return array('ROLE_USER_SIMPLE');
+	}
+
+	public function getSalt()
+	{
+		return null;
+	}
+
+	public function eraseCredentials()
+	{
+		// TODO: Implement eraseCredentials() method.
+	}
+
+
 }
