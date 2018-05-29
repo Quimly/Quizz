f5b1736306c71adcdd38b694fb801161bc9f1e2e
Rmy5 <courteaud.remy@gmail.com>
2018-05-29 18:29:36 +0200

    Minor correction

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -141,7 +141,7 @@ class User implements UserInterface, \Serializable
 
 	public function getRoles()
 	{
-		return array('ROLE_USER_SIMPLE');
+		return array('ROLE_USER');
 	}
 
 	public function getSalt()
