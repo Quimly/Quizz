1134243a337e2f1a4a64320633ff907833da30b3
ArnaudRUFFAULT <arnaud.ruffault@hotmail.fr>
2018-06-03 17:01:14 +0200

    correction anecdotique dans controller test

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -10,17 +10,38 @@ use App\Entity\User;
 class TestController extends Controller
 {
     /**
-     * @Route("/test", name="test")
+     * @Route("/test/", name="test")
      */
     public function index()
     {
 	    $users = $this->getDoctrine()->getRepository(User::class);
 
-		$user = $users->find(20);
+		$user = $users->find(24);
 
 	    return $this->render('test/index.html.twig', [
 		    'controller_name' => 'TestController', 'user' => $user
 	    ]);
     }
-
+    
+    /**
+     * @Route("/profile/roleadmin/", name="setadmin")
+     */
+    public function addAdmin()
+    {
+        $user = $this->getUser();
+        
+        $user->setRole(array('ROLE_ADMIN'));
+        
+        $entityManager = $this->getDoctrine()->getManager();
+        
+        $entityManager->persist($user);
+        
+        $entityManager->flush();
+        
+        return $this->render('test/echo.html.twig', [
+           
+        ]);
+    }
+    
 }
+
