0920818366c07b717e98f89dd627ea43515c8b4c
Rmy5 <courteaud.remy@gmail.com>
2018-06-08 15:39:44 +0200

    Added answer deletion

--- assets/js/prototype.js
+++ assets/js/prototype.js
@@ -15,19 +15,36 @@ $(function () {
         // grab the prototype template
         let newWidget = list.attr('data-prototype');
 
+        // newWidget.append('<a href="#" class="remove-tag">x</a>');
+
         console.log(newWidget);
 
         // replace the "__name__" used in the id and name of the prototype
         // with a number that's unique to your emails
         // end name attribute looks like name="contact[emails][2]"
         newWidget = newWidget.replace(/__name__/g, counter);
+
+
         // Increase the counter
         counter++;
         // And store it, the length cannot be used if deleting widgets is allowed
         list.data(' widget-counter', counter);
 
+        $(newWidget).append('<a href="#" class="remove-tag">x</a>');
+
         // create a new list element and add it to the list
         let newElem = $(list.attr('data-widget-tags')).html(newWidget);
+        newElem.append('<a href="#" class="remove-tag">x</a>');
         newElem.appendTo(list);
+
+
+        $('.remove-tag').click(function(e) {
+
+            e.preventDefault();
+
+            $(this).parent().remove();
+
+            return false;
+        });
     });
 });
