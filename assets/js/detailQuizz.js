32d0d22bd9a3f1127600f8a645a8bc887444ee54
ArnaudRUFFAULT <arnaud.ruffault@hotmail.fr>
2018-06-09 17:00:54 +0200

    Ajout js sur la page d'un quizz

--- assets/js/detailQuizz.js
+++ assets/js/detailQuizz.js
@@ -4,16 +4,20 @@ $(function() {
 	
 	$(".container-question").hide();
 	$(".hide-details-question").hide();
+	$(".hide-details-all-questions").hide();
+	
 	$(".container-answer").hide();
 	$(".hide-details-answer").hide();
 	$(".show-details-all-answers").hide();
 	$(".hide-details-all-answers").hide();
 	
+	//__ Event des bouttons qui gerent les questions
 	$(".show-details-question").on('click', function(e){
 		$(this).parent().next(".container-question").show(500);
 		$(this).hide();
 		$(this).siblings(".hide-details-question").show();
 		$(this).siblings(".show-details-all-answers").show();
+		$(this).siblings(".hide-details-all-answers").trigger('click');
 	});
 	
 	$(".hide-details-question").on('click', function(e){
@@ -21,8 +25,24 @@ $(function() {
 		$(this).hide();
 		$(this).siblings(".show-details-question").show();
 		$(this).siblings(".show-details-all-answers").hide();
+		$(this).siblings(".hide-details-all-answers").hide();
 	});
 	
+	$(".show-details-all-questions").on('click', function(e){
+		let questions = $(this).parent().parent().parent().siblings("div.question-wrap").children("p").children("button.show-details-question");
+		questions.trigger('click');
+		$(this).siblings(".hide-details-all-questions").show();
+		$(this).hide();
+	});
+	
+	$(".hide-details-all-questions").on('click', function(e){
+		let questions = $(this).parent().parent().parent().siblings("div.question-wrap").children("p").children("button.hide-details-question");
+		questions.trigger('click');
+		$(this).siblings(".show-details-all-questions").show();
+		$(this).hide();
+	});
+	
+	//__ Event des bouttons qui gerent les réponses
 	$(".show-details-answer").on('click', function(e){
 		$(this).parent().next(".container-answer").show(500);
 		$(this).hide();
@@ -40,7 +60,7 @@ $(function() {
 	$(".show-details-all-answers").on('click', function(e){
 		let answers = $(this).parent().parent().children(".container-question").children(".answer-wrap").children("p").children("button.show-details-answer");
 		answers.trigger('click');
-		$(this).siblings(".hide-details-all-answers").show();
+		$(this).siblings(".show-details-all-answers").show();
 		$(this).hide();
 	});
 	
