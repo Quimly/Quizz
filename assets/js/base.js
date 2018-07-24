f5722addec74948f137e7a8ad8a9dca8242fab94
Rmy5 <courteaud.remy@gmail.com>
2018-07-24 13:42:26 +0200

    Added bootstrap from node_modules (run webpack!)

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,5 +1,8 @@
 require('../css/base.css');
 
+const $ = require('jquery');
+
+require('bootstrap'); // Bootstrap inclu selon la doc SF: https://symfony.com/doc/current/frontend/encore/bootstrap.html
 
 $(function() {
 
